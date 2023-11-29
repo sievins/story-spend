6e0255f40049934cabdfdd2fb4cdfceda1e24eb1
Sam Ievins <sievins8012@gmail.com>
2023-11-29 19:42:12 +0000

    feat: use clerk to manage authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="btn btn-primary">DaisyUI Button</button>
+      <SignOutButton>
+        <button className="btn btn-primary">Sign out</button>
+      </SignOutButton>
     </main>
   );
 }
