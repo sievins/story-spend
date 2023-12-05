ecf558b8afb67a18f1e8daf51bdb04e180a5d662
Sam Ievins <sievins8012@gmail.com>
2023-12-05 09:36:28 +0000

    feat: menu and drawer scaffold

---
+++ app/components/navbar.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { SignOutButton } from "@clerk/nextjs";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import { drawerID } from "./drawer";
+
+export default function Navbar() {
+  return (
+    <div className="bg-base-100 text-base-content sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur shadow-sm">
+      <nav className="navbar w-full">
+        <div className="flex-none lg:hidden">
+          <label htmlFor={drawerID} className="btn btn-square btn-ghost">
+            <Bars3Icon className="h-6 w-6" />
+          </label>
+        </div>
+        <div className="lg:hidden flex-1">
+          <Link className="btn btn-ghost text-xl" href="/">
+            Story Spend
+          </Link>
+        </div>
+        <div className="flex-1" />
+        <div className="flex-none gap-2">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <details>
+                {/* TODO: Add theme functionality */}
+                <summary className="h-12 content-center">Theme</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <div>Link 1</div>
+                  </li>
+                  <li>
+                    <div>Link 2</div>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+          <SignOutButton>
+            <button className="btn btn-circle">
+              <ArrowLeftOnRectangleIcon className="h-5 w-5" />
+            </button>
+          </SignOutButton>
+        </div>
+      </nav>
+    </div>
+  );
+}
