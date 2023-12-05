456a39c967f9079b9ba4d5cca89252ef4fc916a1
Sam Ievins <sievins8012@gmail.com>
2023-12-05 21:15:14 +0000

    feat: drawer navigation links

--- app/components/drawer.tsx
+++ app/components/drawer.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import { CreditCardIcon } from "@heroicons/react/24/solid";
 import Navbar from "./navbar";
 
 export const drawerID = "my-drawer";
 
+const links = [
+  { name: "Dashboard", href: "/", icon: HomeIcon },
+  { name: "Transactions", href: "/transactions", icon: CreditCardIcon },
+];
+
 export default function Drawer({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
     <div className="drawer lg:drawer-open">
       <input id={drawerID} type="checkbox" className="drawer-toggle" />
@@ -17,20 +30,26 @@ export default function Drawer({ children }: { children: React.ReactNode }) {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <aside className="min-h-screen h-screen flex flex-col w-80">
+        <aside className="min-h-screen h-screen flex flex-col w-60">
           <div className="lg:bg-base-100 bg-base-200 lg:bg-opacity-90 text-base-content sticky top-0 z-30 flex flex-none h-16 backdrop-blur items-center pl-4">
             <Link className="btn btn-ghost text-xl" href="/">
               Story Spend
             </Link>
           </div>
-          <ul className="menu h-full p-4 bg-base-200 text-base-content">
-            {/* TODO: Add sidebar content */}
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+          <ul className="menu h-full p-4 bg-base-200 text-base-content gap-2">
+            {links.map(({ name, href, icon: Icon }) => (
+              <li key={name}>
+                <Link
+                  className={clsx("btn btn-ghost text-lg justify-start", {
+                    active: href === pathname,
+                  })}
+                  href={href}
+                >
+                  <Icon className="h-5 w-5" />
+                  {name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </aside>
       </div>
