bb8703b40114f7dc4c72df3bf4dff441c192123e
Sam Ievins <sievins8012@gmail.com>
2023-12-08 20:07:22 +0000

    feat: edit/delete icons in transactions table

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,4 +1,6 @@
 import { ReceiptRefundIcon } from "@heroicons/react/24/solid";
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 export default function Transactions() {
   return (
@@ -28,6 +30,16 @@ export default function Transactions() {
                       <ReceiptRefundIcon className="h-5 w-5" />
                     </button>
                   </td>
+                  <td>
+                    <div className="flex justify-end gap-2">
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <PencilSquareIcon className="h-5 w-5" />
+                      </button>
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <TrashIcon className="h-5 w-5" />
+                      </button>
+                    </div>
+                  </td>
                 </tr>
                 <tr className="leading-8">
                   <td>Editor - Sarah</td>
@@ -35,6 +47,16 @@ export default function Transactions() {
                   <td>11/04/2023</td>
                   <td>Silent Melody</td>
                   <td />
+                  <td>
+                    <div className="flex justify-end gap-2">
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <PencilSquareIcon className="h-5 w-5" />
+                      </button>
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <TrashIcon className="h-5 w-5" />
+                      </button>
+                    </div>
+                  </td>
                 </tr>
                 <tr className="leading-8">
                   <td>Brice Swyre</td>
@@ -45,6 +67,16 @@ export default function Transactions() {
                     <button className="btn btn-ghost btn-xs align-middle">
                       <ReceiptRefundIcon className="h-5 w-5" />
                     </button>
+                  </td>
+                  <td>
+                    <div className="flex justify-end gap-2">
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <PencilSquareIcon className="h-5 w-5" />
+                      </button>
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <TrashIcon className="h-5 w-5" />
+                      </button>
+                    </div>
                   </td>
                 </tr>
                 <tr className="leading-8">
@@ -57,34 +89,15 @@ export default function Transactions() {
                       <ReceiptRefundIcon className="h-5 w-5" />
                     </button>
                   </td>
-                </tr>
-                <tr className="leading-8">
-                  <td>Editor - Sarah</td>
-                  <td>£204.12</td>
-                  <td>11/04/2023</td>
-                  <td>Silent Melody</td>
-                  <td />
-                </tr>
-                <tr className="leading-8">
-                  <td>Brice Swyre</td>
-                  <td>£300</td>
-                  <td>04/01/2021</td>
-                  <td />
                   <td>
-                    <button className="btn btn-ghost btn-xs align-middle">
-                      <ReceiptRefundIcon className="h-5 w-5" />
-                    </button>
-                  </td>
-                </tr>
-                <tr className="leading-8">
-                  <td>Publisher</td>
-                  <td>£7.12</td>
-                  <td>12/02/2024</td>
-                  <td>Feathers of Snow</td>
-                  <td>
-                    <button className="btn btn-ghost btn-xs align-middle">
-                      <ReceiptRefundIcon className="h-5 w-5" />
-                    </button>
+                    <div className="flex justify-end gap-2">
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <PencilSquareIcon className="h-5 w-5" />
+                      </button>
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <TrashIcon className="h-5 w-5" />
+                      </button>
+                    </div>
                   </td>
                 </tr>
                 <tr className="leading-8">
@@ -93,16 +106,15 @@ export default function Transactions() {
                   <td>11/04/2023</td>
                   <td>Silent Melody</td>
                   <td />
-                </tr>
-                <tr className="leading-8">
-                  <td>Brice Swyre</td>
-                  <td>£300</td>
-                  <td>04/01/2021</td>
-                  <td />
                   <td>
-                    <button className="btn btn-ghost btn-xs align-middle">
-                      <ReceiptRefundIcon className="h-5 w-5" />
-                    </button>
+                    <div className="flex justify-end gap-2">
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <PencilSquareIcon className="h-5 w-5" />
+                      </button>
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <TrashIcon className="h-5 w-5" />
+                      </button>
+                    </div>
                   </td>
                 </tr>
                 <tr className="leading-8">
@@ -114,6 +126,16 @@ export default function Transactions() {
                     <button className="btn btn-ghost btn-xs align-middle">
                       <ReceiptRefundIcon className="h-5 w-5" />
                     </button>
+                  </td>
+                  <td>
+                    <div className="flex justify-end gap-2">
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <PencilSquareIcon className="h-5 w-5" />
+                      </button>
+                      <button className="btn btn-ghost btn-xs align-middle">
+                        <TrashIcon className="h-5 w-5" />
+                      </button>
+                    </div>
                   </td>
                 </tr>
               </tbody>
