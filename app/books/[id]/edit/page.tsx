ec6c07bfee7b3721fc5ca2adeeeaa747e01440bb
Sam Ievins <sievins8012@gmail.com>
2024-08-17 22:04:45 +0100

    Skeleton for editing book

--- app/books/[id]/edit/page.tsx
+++ app/books/[id]/edit/page.tsx
@@ -1,10 +1,23 @@
-import Error from "@/components/error";
+import { Suspense } from "react";
 import Form from "@/app/books/components/form";
 import { fetchBookById } from "@/data";
 import { notFound } from "next/navigation";
-import type { Book } from "@prisma/client";
 
-export default async function EditBook({ params }: { params: { id: string } }) {
+type Params = { params: { id: string } };
+
+export default async function EditBook({ params }: Params) {
+  return (
+    <>
+      <h1>Edit book</h1>
+      {/* No need for the Error component because of redirect to not found page. */}
+      <Suspense fallback={<FormSkeleton />}>
+        <FormWrapper params={params} />
+      </Suspense>
+    </>
+  );
+}
+
+async function FormWrapper({ params }: Params) {
   const id = params.id;
 
   const book = await fetchBookById(id);
@@ -13,16 +26,17 @@ export default async function EditBook({ params }: { params: { id: string } }) {
     notFound();
   }
 
-  return (
-    <>
-      <h1>Edit book</h1>
-      <Error>
-        <FormWrapper book={book} />
-      </Error>
-    </>
-  );
+  return <Form book={book} />;
 }
 
-async function FormWrapper({ book }: { book: Book }) {
-  return <Form book={book} />;
+function FormSkeleton() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col gap-y-11 w-full max-w-sm mt-9">
+        <div className="skeleton h-12"></div>
+      </div>
+      <div className="h-6" />
+      <div className="skeleton h-12 w-44"></div>
+    </div>
+  );
 }
